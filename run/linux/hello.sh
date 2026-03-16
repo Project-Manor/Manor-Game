cf33f4dd73ae1d29c0d7de96772e0711f90dc5d8
q4niel <git.unrigged621@passmail.com>
2026-03-16 20:20:02 +0100

    fix 'hello.sh' linux runner relative path issue, add auto removal of built docker image

--- run/linux/hello.sh
+++ run/linux/hello.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 clear
-cd "$(realpath -m "$0/../..")"
+cd "$(realpath -m "$0/../../..")"
+sudo docker rmi -f manor-game-env:latest
 sudo docker buildx build -t manor-game-env:latest -f ./env/Dockerfile.linux .
 sudo docker run manor-game-env:latest "env/src/hello.py"
+sudo docker rmi -f manor-game-env:latest

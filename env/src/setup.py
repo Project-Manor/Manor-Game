51defac4eacd1462e87ea50c2f710512ded63cee
q4niel <git.unrigged621@passmail.com>
2026-03-27 21:50:46 +0100

    add vendor installation to setup runner

--- env/src/setup.py
+++ env/src/setup.py
@@ -1,4 +1,33 @@
 import os
+import platform
+import requests
+import tarfile
+from typing import List
+import tomllib as toml
 
 if __name__ == "__main__":
-    os.mkdir("vnd")
+    vendorDir: str = "vnd"
+    tmpDir: str = "tmp"
+    os.mkdir(vendorDir)
+    os.mkdir(tmpDir)
+
+    renamedLibs: List[str] = []
+
+    with open("env/vnd.toml", "rb") as f:
+        config: dict = toml.load(f)
+    
+        for key, value in config["vendors"].items():
+            print(f"Installing {key}")
+            with open(f"{tmpDir}/{key}", 'wb') as file:
+                file.write (
+                    requests.get(value[platform.system()]).content
+                )
+
+            print(f"Extracting {key}")
+            with tarfile.open(f"{tmpDir}/{key}", "r:*") as tar:
+                tar.extractall(path=f"{vendorDir}")
+
+            for path in os.listdir(vendorDir):
+                if path not in renamedLibs:
+                    os.rename(f"{vendorDir}/{path}", f"{vendorDir}/{key}")
+                    renamedLibs.append(key)

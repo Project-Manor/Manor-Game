a2ffe0a722e2190cdfed5053c295d3509afa23ad
q4niel <git.unrigged621@passmail.com>
2026-04-24 20:23:13 +0200

    3D camera initialization with abstracted getters/setters within Renderer

--- src/man/render/renderer.cxx
+++ src/man/render/renderer.cxx
@@ -1,5 +1,5 @@
 #include "renderer.hxx"
-#include <raylib.h>
+#include "raylib.h"
 #include "renderable.hxx"
 
 namespace man::render {
@@ -8,20 +8,24 @@ namespace man::render {
         return inst;
     }
 
-    const bool Renderer::isAlive() {
-        return instance()._isAlive;
-    }
+    const bool Renderer::isAlive()
+    { return instance()._isAlive; }
 
-    const int Renderer::getFPS() {
-        return GetFPS();
-    }
+    const int Renderer::getFPS()
+    { return GetFPS(); }
 
-    void Renderer::setFPS(int value) {
-        SetTargetFPS(value);
-    }
+    void Renderer::setFPS(int value)
+    { SetTargetFPS(value); }
 
     Renderer::Renderer() :
         _isAlive(true),
+        _cam({
+            .position = {.0f, .0f, .0f},
+            .target = {.0f, .0f, .0f},
+            .up = {.0f, .0f, .0f},
+            .fovy = .0f,
+            .projection = CAMERA_PERSPECTIVE
+        }),
         _nextRenderIndex(0),
         _renders({})
     {
@@ -41,10 +45,12 @@ namespace man::render {
 
         BeginDrawing();
         ClearBackground(SKYBLUE);
+        BeginMode3D(_cam);
 
         for (auto &[index, render] : _renders)
             render->draw();
 
+        EndMode3D();
         EndDrawing();
     }
 
@@ -54,12 +60,118 @@ namespace man::render {
         return inst._nextRenderIndex++;
     }
 
-    void Renderer::removeRenderable(const RenderIndex renderIndex) {
-        instance()._renders.erase(renderIndex);
-    }
+    void Renderer::removeRenderable(const RenderIndex renderIndex)
+    { instance()._renders.erase(renderIndex); }
 
     void Renderer::_term() {
         CloseWindow();
         instance()._isAlive = false;
+    }
+
+    // Camera FOV
+    const float Renderer::Camera::getFOV()
+    { return instance()._cam.fovy; }
+
+    void Renderer::Camera::setFOV(float val)
+    { instance()._cam.fovy = val; }
+
+    // Camera Projection
+    const float Renderer::Camera::getProjection()
+    { return instance()._cam.projection; }
+
+    void Renderer::Camera::setProjection(int val)
+    { instance()._cam.projection = val; }
+
+    // Camera Position
+    const float Renderer::Camera::getPosX()
+    { return instance()._cam.position.x; }
+
+    void Renderer::Camera::setPosX(float val)
+    { instance()._cam.position.x = val; }
+
+    const float Renderer::Camera::getPosY()
+    { return instance()._cam.position.y; }
+
+    void Renderer::Camera::setPosY(float val)
+    { instance()._cam.position.y = val; }
+
+    const float Renderer::Camera::getPosZ()
+    { return instance()._cam.position.z; }
+
+    void Renderer::Camera::setPosZ(float val)
+    { instance()._cam.position.z = val; }
+
+    const Vector3 Renderer::Camera::getPos() { return {
+        getPosX(),
+        getPosY(),
+        getPosZ()
+    };}
+
+    void Renderer::Camera::setPos(Vector3 vec) {
+        setPosX(vec.x);
+        setPosY(vec.y);
+        setPosZ(vec.z);
+    }
+
+    // Camera Target
+    const float Renderer::Camera::getTargetX()
+    { return instance()._cam.target.x; }
+
+    void Renderer::Camera::setTargetX(float val)
+    { instance()._cam.target.x = val; }
+
+    const float Renderer::Camera::getTargetY()
+    { return instance()._cam.target.y; }
+
+    void Renderer::Camera::setTargetY(float val)
+    { instance()._cam.target.y = val; }
+
+    const float Renderer::Camera::getTargetZ()
+    { return instance()._cam.target.z; }
+
+    void Renderer::Camera::setTargetZ(float val)
+    { instance()._cam.target.z = val; }
+
+    const Vector3 Renderer::Camera::getTarget() { return {
+        getTargetX(),
+        getTargetY(),
+        getTargetZ()
+    };}
+
+    void Renderer::Camera::setTarget(Vector3 vec) {
+        setTargetX(vec.x);
+        setTargetY(vec.y);
+        setTargetZ(vec.z);
+    }
+
+    // Camera Up
+    const float Renderer::Camera::getUpX()
+    { return instance()._cam.up.x; }
+
+    void Renderer::Camera::setUpX(float val)
+    { instance()._cam.up.x = val; }
+
+    const float Renderer::Camera::getUpY()
+    { return instance()._cam.up.y; }
+
+    void Renderer::Camera::setUpY(float val)
+    { instance()._cam.up.y = val; }
+
+    const float Renderer::Camera::getUpZ()
+    { return instance()._cam.up.z; }
+
+    void Renderer::Camera::setUpZ(float val)
+    { instance()._cam.up.z = val; }
+
+    const Vector3 Renderer::Camera::getUp() { return {
+        getUpX(),
+        getUpY(),
+        getUpZ()
+    };}
+
+    void Renderer::Camera::setUp(Vector3 vec) {
+        setUpX(vec.x);
+        setUpY(vec.y);
+        setUpZ(vec.z);
     }
 }
--- src/man/render/renderer.hxx
+++ src/man/render/renderer.hxx
@@ -1,5 +1,6 @@
 #pragma once
 #include <unordered_map>
+#include <raylib.h>
 #include "../core.hxx"
 #include "render_types.hxx"
 #include "renderable.hxx"
@@ -17,6 +18,53 @@ namespace man::render {
         static const RenderIndex addRenderable(Renderable *render);
         static void removeRenderable(const RenderIndex renderIndex);
 
+        static struct Camera {
+            const float getFOV();
+            void setFOV(float val);
+
+            const float getProjection();
+            void setProjection(int val);
+
+            // Position
+            const float getPosX();
+            void setPosX(float val);
+
+            const float getPosY();
+            void setPosY(float val);
+
+            const float getPosZ();
+            void setPosZ(float val);
+
+            const Vector3 getPos();
+            void setPos(Vector3 vec);
+
+            // Target
+            const float getTargetX();
+            void setTargetX(float val);
+
+            const float getTargetY();
+            void setTargetY(float val);
+
+            const float getTargetZ();
+            void setTargetZ(float val);
+
+            const Vector3 getTarget();
+            void setTarget(Vector3 vec);
+
+            // Up
+            const float getUpX();
+            void setUpX(float val);
+
+            const float getUpY();
+            void setUpY(float val);
+
+            const float getUpZ();
+            void setUpZ(float val);
+
+            const Vector3 getUp();
+            void setUp(Vector3 vec);
+        } camera;
+
     friend bool man::proc();
 
     private:
@@ -25,6 +73,7 @@ namespace man::render {
         void _term();
 
         bool _isAlive;
+        Camera3D _cam;
 
         RenderIndex _nextRenderIndex;
         std::unordered_map <

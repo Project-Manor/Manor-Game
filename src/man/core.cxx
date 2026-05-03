02db94b367ed9e2f11875a6a0fab18426dc2baac
q4niel <git.unrigged621@passmail.com>
2026-05-03 21:46:08 +0200

    initialize things singleton

--- src/man/core.cxx
+++ src/man/core.cxx
@@ -2,15 +2,18 @@
 #include <raylib.h>
 #include "time.hxx"
 #include "render/renderer.hxx"
+#include "things/things.hxx"
 
 void man::init() {
     Time::instance();
     render::Renderer::instance();
+    man::Things::instance();
 }
 
 bool man::proc() {
     Time::instance()._proc();
     render::Renderer::instance()._proc();
+    man::Things::instance()._proc();
     return render::Renderer::isAlive();
 }
 

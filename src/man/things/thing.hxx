eb490ffab291dbb0473a5669e4691f09edf8748d
q4niel <120566282+q4niel@users.noreply.github.com>
2026-05-04 21:51:29 +0000

    [main] <- [things] (#6)
    * add returner to Things::create
    
    * re-implement thing tagging

--- src/man/things/thing.hxx
+++ src/man/things/thing.hxx
@@ -1,16 +1,10 @@
 #pragma once
-#include <string_view>
-
-#define UNTAGGED_THING "UNTAGGED_THING"
-#define TAG(name) static constexpr std::string_view tag = #name
 
 namespace man::things {
     class Thing {
     public:
         Thing();
         virtual ~Thing();
-
-        static constexpr std::string_view tag = UNTAGGED_THING;
 
         virtual void launch() = 0;
         virtual void process() = 0;
--- src/man/things/things.t.hxx
+++ src/man/things/things.t.hxx
@@ -1,13 +1,46 @@
 #pragma once
 #include "things.hxx"
+#include "../print.hxx"
 
 template<typename T>
 requires std::derived_from<T, man::things::Thing>
-void man::Things::create() {
+std::optional<std::reference_wrapper<T>> man::Things::create() {
     T *t = new T;
+    static_cast<T*>(t)->launch();
 
-    if (T::tag == UNTAGGED_THING) {
-        instance()._uThingCtrs.emplace_back (
+    instance()._uThingCtrs.emplace_back (
+        t,
+        [] (void *ptr) {
+            static_cast<T*>(ptr)->process();
+        },
+        [] (void *ptr) {
+            static_cast<T*>(ptr)->finish();
+            delete static_cast<T*>(ptr);
+        }
+    );
+
+    return std::ref (
+        *static_cast<T*>(instance()._uThingCtrs.back().ptr)
+    );
+}
+
+template<typename T>
+requires std::derived_from<T, man::things::Thing>
+std::optional<std::reference_wrapper<T>> man::Things::create(std::string tag) {
+    if (instance()._tThingCtrs.contains(tag)) {
+        printspace();
+        println("Things Error: A thing with tag '", tag, "' already exists!");
+        println("            : Thing was not created.");
+        printspace();
+        return {std::nullopt};
+    }
+
+    T *t = new T;
+    static_cast<T*>(t)->launch();
+
+    instance()._tThingCtrs.insert ({
+        tag,
+        {
             t,
             [] (void *ptr) {
                 static_cast<T*>(ptr)->process();
@@ -16,37 +49,26 @@ void man::Things::create() {
                 static_cast<T*>(ptr)->finish();
                 delete static_cast<T*>(ptr);
             }
-        );
-    }
-    else {
-        instance()._tThingCtrs.insert ({
-            T::tag,
-            {
-                t,
-                [] (void *ptr) {
-                    static_cast<T*>(ptr)->process();
-                },
-                [] (void *ptr) {
-                    static_cast<T*>(ptr)->finish();
-                    delete static_cast<T*>(ptr);
-                }
-            }
-        });
-    }
+        }
+    });
 
-    static_cast<T*>(t)->launch();
+    return std::ref (
+        *static_cast<T*>(instance()._tThingCtrs[tag].ptr)
+    );
 }
 
 template<typename T>
 requires std::derived_from<T, man::things::Thing>
-std::optional<std::reference_wrapper<T>> man::Things::getTagged() {
-    Things &inst = instance();
-
-    if (inst._tThingCtrs.contains(T::tag)) return {
+std::optional<std::reference_wrapper<T>> man::Things::getTagged(std::string tag) {
+    if (instance()._tThingCtrs.contains(tag)) return {
         std::ref (
-            *static_cast<T*>(inst._tThingCtrs[T::tag].ptr)
+            *static_cast<T*>(instance()._tThingCtrs[tag].ptr)
         )
     };
 
+    printspace();
+    println("Things Warning: A thing with tag '", tag, "' does not exist!");
+    println("              : Returning 'null option'.");
+    printspace();
     return {std::nullopt};
 }

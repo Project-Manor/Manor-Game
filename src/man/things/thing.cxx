bb4192fb0fcee34cf3b2083e1a15d247d1b90b8e
q4niel <git.unrigged621@passmail.com>
2026-05-05 15:55:40 +0200

    new event system for Thing

--- src/man/things/thing.cxx
+++ src/man/things/thing.cxx
@@ -1,5 +1,13 @@
 #include "thing.hxx"
 
-man::things::Thing::Thing() = default;
+man::things::Thing::Thing() :
+    _inits(0),
+    _procs(0),
+    _terms(0)
+{}
 
 man::things::Thing::~Thing() = default;
+
+void man::things::Thing::launch() {}
+void man::things::Thing::process() {}
+void man::things::Thing::finish() {}
--- src/man/things/thing.hxx
+++ src/man/things/thing.hxx
@@ -1,13 +1,43 @@
 #pragma once
+#include <vector>
+#include <functional>
+
+namespace man {
+    class Things;
+}
 
 namespace man::things {
     class Thing {
+    friend class man::Things;
     public:
         Thing();
         virtual ~Thing();
 
-        virtual void launch() = 0;
-        virtual void process() = 0;
-        virtual void finish() = 0;
+        virtual void init() = 0;
+
+        virtual void launch();
+        virtual void process();
+        virtual void finish();
+
+    protected:
+        template<typename T>
+        void _addInit(T *obj, void (T::*meth)()) const {
+            _inits.emplace_back(std::bind_front(meth, obj));
+        }
+
+        template<typename T>
+        void _addProc(T *obj, void (T::*meth)()) const {
+            _procs.emplace_back(std::bind_front(meth, obj));
+        }
+
+        template<typename T>
+        void _addTerm(T *obj, void (T::*meth)()) const {
+            _terms.emplace_back(std::bind_front(meth, obj));
+        }
+
+    private:
+        mutable std::vector<std::function<void()>> _inits;
+        mutable std::vector<std::function<void()>> _procs;
+        mutable std::vector<std::function<void()>> _terms;
     };
 }

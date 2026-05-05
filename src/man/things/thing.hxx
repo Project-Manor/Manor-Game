17d0d038e621807af213f97765d7dce7b2ab63a9
q4niel <git.unrigged621@passmail.com>
2026-05-05 16:42:07 +0200

    add Doxygen to 'thing.hxx'

--- src/man/things/thing.hxx
+++ src/man/things/thing.hxx
@@ -13,6 +13,9 @@ namespace man::things {
         Thing();
         virtual ~Thing();
 
+        /**
+        *   @brief Runs once on Thing's creation
+        */
         virtual void init() = 0;
 
         virtual void launch();
@@ -20,16 +23,31 @@ namespace man::things {
         virtual void finish();
 
     protected:
+        /**
+        *   @brief Add a Initialization function, runs sequentially on Thing's creation
+        *   @param obj Pointer to the method owner, pass 'this'
+        *   @param meth Pointer to the method, pass '&MyThing::myMethod'
+        */
         template<typename T>
         void _addInit(T *obj, void (T::*meth)()) const {
             _inits.emplace_back(std::bind_front(meth, obj));
         }
 
+        /**
+        *   @brief Add a Process function, loops sequentially during Thing's lifetime
+        *   @param obj Pointer to the method owner, pass 'this'
+        *   @param meth Pointer to the method, pass '&MyThing::myMethod'
+        */
         template<typename T>
         void _addProc(T *obj, void (T::*meth)()) const {
             _procs.emplace_back(std::bind_front(meth, obj));
         }
 
+        /**
+        *   @brief Add a Termination function, runs sequentially on Thing's destruction
+        *   @param obj Pointer to the method owner, pass 'this'
+        *   @param meth Pointer to the method, pass '&MyThing::myMethod'
+        */
         template<typename T>
         void _addTerm(T *obj, void (T::*meth)()) const {
             _terms.emplace_back(std::bind_front(meth, obj));

6ef95f48c9a990dfc62cb914cc1dc77f3ca2ce52
q4niel <120566282+q4niel@users.noreply.github.com>
2026-06-02 21:15:44 +0000

    [main] <- [docker] (#12)
    * Glowmouse added docker as an alternative to podman, I guess he prefers software with security vulnarabilities?
    
    * add early exit when no container engine has been set

--- env/run/.inc.sh
+++ env/run/.inc.sh
@@ -1,27 +1,49 @@
 #!/bin/bash
 
+containerEngine=$(head -n 1 ./env/container_engine)
+missingContainerEngineError="Hello, this is Lord Glowmouse hehe...\nI can't seem to find my 'container_engine'\nI left in 'env/'\nI wrote something really important in there, can't seem to remember what...\nWas it 'podman', was it 'docker', idk...\nFind it for me, and I'll give you a lil' bite... hehe..."
 imageName=manor-game-env
 containerFile=./env/Containerfile
 
 deleteImage() {
-    # if [ "$(sudo docker ps -a -q -f name=^${imageName})" ]; then
-    #     sudo docker rm -f $imageName
-    # fi
-    podman rmi -f $imageName:latest
+    if [[ $containerEngine == podman ]]; then
+        podman rmi -f $imageName:latest
+    elif [[ $containerEngine == docker ]]; then
+        if [ "$(sudo docker ps -a -q -f name=^${imageName})" ]; then
+            sudo docker rm -f $imageName
+        fi
+    else
+        echo -e $missingContainerEngineError
+        exit 1
+    fi
 }
 
 buildImage() {
     deleteImage
-    # sudo docker buildx build -t $imageName:latest -f ./env/Dockerfile.linux .
-    podman build -f $containerFile -t $imageName:latest .
+
+    if [[ $containerEngine == podman ]]; then
+        podman build -f $containerFile -t $imageName:latest .
+    elif [[ $containerEngine == docker ]]; then
+        sudo docker buildx build -t $imageName:latest -f $containerFile .
+    else
+        echo -e $missingContainerEngineError
+        exit 1
+    fi
 }
 
 runImage() {
     local pyScript=$1
     local pyArgs=("${@:2}")
     buildImage
-    # sudo docker run --name $imageName $imageName:latest $pyScript ${pyArgs[@]}
-    podman run --replace --name $imageName $imageName:latest $pyScript ${pyArgs[@]}
+
+    if [[ $containerEngine == podman ]]; then
+        podman run --replace --name $imageName $imageName:latest $pyScript ${pyArgs[@]}
+    elif [[ $containerEngine == docker ]]; then
+        sudo docker run --name $imageName $imageName:latest $pyScript ${pyArgs[@]}
+    else
+        echo -e $missingContainerEngineError
+        exit 1
+    fi
 }
 
 copyImageData() {
@@ -40,7 +62,14 @@ copyImageData() {
         mkdir "./$outDir"
     fi
 
-    # sudo docker cp "$imageName:/$dataPath" "./$outDir"
-    podman cp "$imageName:/$dataPath" "./$outDir"
+    if [[ $containerEngine == podman ]]; then
+        podman cp "$imageName:/$dataPath" "./$outDir"
+    elif [[ $containerEngine == docker ]]; then
+        sudo docker cp "$imageName:/$dataPath" "./$outDir"
+    else
+        echo -e $missingContainerEngineError
+        exit 1
+    fi
+
     # sudo chown -R --reference=. "./$outDir"
 }

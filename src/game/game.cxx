39eec4b9debd4da4056f7d5eea907d58b268f96a
q4niel <git.unrigged621@passmail.com>
2026-06-05 21:13:25 +0200

    remove maxie bloat

--- src/game/game.cxx
+++ src/game/game.cxx
@@ -1,16 +1,11 @@
 #include "game.hxx"
 #include "../man/things/things.hxx"
-#include "room.hxx"
-#include "player.hxx"
 #include "debug_fp_cam.hxx"
-#include "npc.hxx"
 
 void game::init() {
 #ifdef DEBUG
     auto debug_cam = man::Things::create<DebugFPCamera>();
 #endif
-    auto player = man::Things::create<Player>();
-    auto npc = man::Things::create<NPC>();
 }
 
 void game::proc() {}
